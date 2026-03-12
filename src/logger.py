526036ae51ef279cf00d042205096a0253aa22dd
umar Awan <Umarfarooq6153@gmail.com>
2026-03-12 17:22:41 +0500

    fix:bug

--- src/logger.py
+++ src/logger.py
@@ -1,19 +1,18 @@
-#  custom exceptional handling in python
+import logging
+import os
+from datetime import datetime
 
-import sys
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-def error_message_detail(error, error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in Python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
-    return error_message                         
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
-class CustomeException(Exception):                
-    def __init__(self, error_message, error_detail:sys):
-        super().__init__(error_message)           
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
-    
-    def __str__(self):                           
-        return self.error_message
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
