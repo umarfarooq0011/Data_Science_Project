656b2d0b5636fcfe1d75c0d40244bf3299ee3992
umar Awan <Umarfarooq6153@gmail.com>
2026-03-13 15:42:54 +0500

    feat:Add EDA and model training notebooks

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-       requirements = [req.replace("\n","") for req in requirements]
+        requirements = [req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
